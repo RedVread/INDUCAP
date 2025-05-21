959aa08912b82a6335cd659d3346d2f1dc5e4424
RedBread <wassim1567@gmail.com>
2025-05-21 23:18:26 +0200

    Added loading cars to car page

--- PHP/loadAllCars.php
+++ PHP/loadAllCars.php
@@ -1,6 +1,32 @@
 <?php 
 
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "incuap-cotxes";
 
+// Create a connection
+$conn = mysqli_connect($host, $user, $password, $database);
 
+// Check the connection
+if (!$conn) {
+    die("Failed connection to database: " . mysqli_connect_error());
+}
+
+$result = $conn->query("SELECT * FROM cotxes");
+
+if ($result->num_rows > 0) {
+
+    for ($i = 0; $i<$result->num_rows; $i++){
+
+        $row = $result->fetch_assoc();
+        
+        echo "Fabricant = " . $row["fabricant"] . "<br/>";
+
+    }
+}
+
+//Cerrar la conexión
+mysqli_close($conn);
 
 ?>
