7240682ffe9e3e774e561032b4d46a8b7e4b458b
RedBread <wassim1567@gmail.com>
2025-05-22 12:34:04 +0200

    Synch commit

--- pages/furgonetes.php
+++ pages/furgonetes.php
@@ -7,8 +7,9 @@
     <title>INCUAP | Cotxes clàssics a preus d'escàndol</title>
 </head>
 <body>
+
     <div class="bg-white">
-        <header class="absolute inset-x-0 top-0 z-50">
+        <header class="absolute inset-x-0 top-0 z-50 my-1">
           <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div class="flex lg:flex-1">
               <a href="#" class="-m-1.5 p-1.5">
@@ -18,14 +19,14 @@
             </div>
             <div class="flex lg:hidden">
               <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-                <span class="sr-only">Open main menu</span>
+                <span class="sr-only">Menú principal</span>
                 <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
               </button>
             </div>
             <div class="hidden lg:flex lg:gap-x-12">
-              <a href="#" class="text-sm/6 font-semibold text-gray-900">Cotxes</a>
+              <a href="#" class="text-sm/6 font-semibold text-blue-700">Cotxes</a>
               <a href="#" class="text-sm/6 font-semibold text-gray-900">Furgonetes</a>
               <a href="#" class="text-sm/6 font-semibold text-gray-900">Solicita un cotxe</a>
               <a href="#" class="text-sm/6 font-semibold text-gray-900">Porta'ns el teu cotxe</a>
@@ -54,7 +55,7 @@
               <div class="mt-6 flow-root">
                 <div class="-my-6 divide-y divide-gray-500/10">
                   <div class="space-y-2 py-6">
-                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Cotxes</a>
+                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-blue-700 hover:bg-gray-50">Cotxes</a>
                     <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Furgonetes</a>
                     <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Sol·licita un cotxe</a>
                     <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Porta'ns el teu cotxe</a>
@@ -69,19 +70,39 @@
         </header>
       
         <div class="relative isolate px-6 pt-14 lg:px-8">
-          <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+
+
+          <div class="grid grid-cols-3 gap-5 absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
             <div class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
           </div>
-          <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-            <div class="text-center">
-              <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Somia el teu cotxe. Condueix-lo.</h1>
-              <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Tenim una selecció de cotxes clàssics tan gran, que estem segurs que tenim el que estàs buscant.</p>
-              <div class="mt-10 flex items-center justify-center gap-x-6">
-                <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Explora</a>
-                <a href="#" class="text-sm/6 font-semibold text-gray-900">Buscar<span aria-hidden="true">→</span></a>
-              </div>
-            </div>
+
+          <div class="grid grid-cols-4 gap-3 my-10">
+          <?php 
+              include '../PHP/loadAllCars.php'; 
+
+              foreach ($cotxes as $cotxe){
+
+                echo '
+                      <div class="max-w-sm rounded overflow-hidden shadow-lg" carid="' . $cotxe["id"] . '">
+                        <img class="w-full" src="' . $cotxe["fotos"] . '" alt="Mountain">
+                        <div class="px-6 py-4">
+                          <div class="font-bold text-xl mb-2">' . $cotxe["fabricant"] . ' ' . $cotxe["model"] . ' ' . $cotxe["any"] .'</div>
+                          <p class="text-gray-700 text-base">' .
+                            $cotxe["color"]
+                          .'</p>
+                        </div>
+                        <!-- <div class="px-6 pt-4 pb-2">
+                          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                        </div> -->
+                      </div>';
+
+              }
+
+            ?>
           </div>
+          
           <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
             <div class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
           </div>
