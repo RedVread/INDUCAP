2db5d91bdcb46d2668022e1928944acd99856555
RedBread <wassim1567@gmail.com>
2025-07-08 14:49:13 +0200

    Put a header in a single file that is included when needed.

--- pages/furgoneta.php
+++ pages/furgoneta.php
@@ -9,48 +9,46 @@
 <body>
 
     <div class="bg-white">
-    <?php include "../PHP/header.php"; ?>
+
+      <?php include "../PHP/header.php"; ?>
+        
+      <div class="relative isolate px-6 pt-14 lg:px-8">
+
+      <div class="grid grid-cols-3 gap-5 absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+        <div class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+      </div>
+              
+      <?php 
       
-  <div class="relative isolate px-6 pt-14 lg:px-8">
+      (isset($_GET["id"])) && $vanId = $_GET["id"];
 
-    <div class="grid grid-cols-3 gap-5 absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-      <div class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-    </div>
-            
-    <?php 
-     
-    (isset($_GET["id"])) && $vanId = $_GET["id"];
+      include '../PHP/loadSingleVan.php';
 
-    include '../PHP/loadSingleVan.php';
+      ?>
 
-    ?>
-
-    <div class="flex flex-col">
+      <div class="flex flex-col">
 
         <div class="flex flex-row my-15">
-            <div class="basis-1/3 mx-10">
-                <img src="<?= $furgoneta['fotos']?>" alt="">
-            </div>
-            <div class="basis-2/3">
-                <h3 class="mb-2 mt-0 text-3xl font-medium leading-tight text-primary"><?=$furgoneta["fabricant"] .' '. $furgoneta['model']?></h3>
-                <h5 class="mb-2 mt-0 text-xl font-medium leading-tight text-primary">Any: <?=$furgoneta["any"]?></h5>
-                <h5 class="mb-2 mt-0 text-xl font-medium leading-tight text-primary">Color: <?=$furgoneta["color"]?></h5>
-                <h5 class="mb-2 mt-0 text-xl font-medium leading-tight text-primary">Kilòmetres: <?=$furgoneta["kms"] . 'km'?></h5>
-            </div>
 
-        </div>
+              <div class="basis-1/3 mx-10">
+                  <img src="<?= $furgoneta['fotos']?>" alt="">
+              </div>
+              <div class="basis-2/3">
+                  <h3 class="mb-2 mt-0 text-3xl font-medium leading-tight text-primary"><?=$furgoneta["fabricant"] .' '. $furgoneta['model']?></h3>
+                  <h5 class="mb-2 mt-0 text-xl font-medium leading-tight text-primary">Any: <?=$furgoneta["any"]?></h5>
+                  <h5 class="mb-2 mt-0 text-xl font-medium leading-tight text-primary">Color: <?=$furgoneta["color"]?></h5>
+                  <h5 class="mb-2 mt-0 text-xl font-medium leading-tight text-primary">Kilòmetres: <?=$furgoneta["kms"] . 'km'?></h5>
+              </div>
+              
+              <div class="mx-10">
+                  <p><?=$furgoneta["descripcio"]?></p>
+              </div>
 
-        <div class="mx-10">
-            <p><?=$furgoneta["descripcio"]?></p>
-        </div>
-
-    </div>
-    
-          
-          <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-            <div class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-          </div>
+              <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+                <div class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+              </div>
         </div>
       </div>
+    </div>
 </body>
 </html>
--- pages/furgonetes.php
+++ pages/furgonetes.php
@@ -11,78 +11,20 @@
 <body>
 
     <div class="bg-white">
-        <header class="absolute inset-x-0 top-0 z-50 my-1">
-          <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-            <div class="flex lg:flex-1">
-              <a href="#" class="-m-1.5 p-1.5">
-                <span class="sr-only">INDUCAP</span>
-                <div class="flex items-center justify-between">
-                  <img class="h-8 w-auto" src="https://cdn-icons-png.freepik.com/512/5266/5266272.png" alt="">&nbsp;
-                  <h1>INDUCAP</h1>
-                </div>
-              </a>
-            </div>
-            <div class="flex lg:hidden">
-              <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-                <span class="sr-only">Menú principal</span>
-                <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg>
-              </button>
-            </div>
-            <div class="hidden lg:flex lg:gap-x-12">
-              <a href="#" class="text-sm/6 font-semibold text-gray-900">Cotxes</a>
-              <a href="#" class="text-sm/6 font-semibold text-blue-700">Furgonetes</a>
-              <a href="#" class="text-sm/6 font-semibold text-gray-900">Solicita un cotxe</a>
-              <a href="#" class="text-sm/6 font-semibold text-gray-900">Porta'ns el teu cotxe</a>
-            </div>
-            <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-              <a href="#" class="text-sm/6 font-semibold text-gray-900"><span aria-hidden="true"></span></a>
-            </div>
-          </nav>
-          <!-- Mobile menu, show/hide based on menu open state. -->
-          <div class="lg:hidden" role="dialog" aria-modal="true">
-            <!-- Background backdrop, show/hide based on slide-over state. -->
-            <div class="fixed inset-0 z-50"></div>
-            <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-              <div class="flex items-center justify-between">
-                <a href="#" class="-m-1.5 p-1.5">
-                  <span class="sr-only">INCUAP</span>
-                  <img class="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="">
-                </a>
-                <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
-                  <span class="sr-only">Close menu</span>
-                  <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                  </svg>
-                </button>
-              </div>
-              <div class="mt-6 flow-root">
-                <div class="-my-6 divide-y divide-gray-500/10">
-                  <div class="space-y-2 py-6">
-                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Cotxes</a>
-                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-blue-700 hover:bg-gray-50">Furgonetes</a>
-                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Sol·licita un cotxe</a>
-                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Porta'ns el teu cotxe</a>
-                  </div>
-                  <div class="py-6">
-                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Contacte</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </header>
+
+    <?php include "../PHP/header.php"; ?>
+
+      <div class="relative isolate px-6 pt-14 lg:px-8">
       
-        <div class="relative isolate px-6 pt-14 lg:px-8">
+      <!-- 
+        <div class="grid grid-cols-3 gap-5 absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+          <div class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+        </div> -->
 
+        <div class="grid grid-cols-4 gap-3 my-10">
 
-          <div class="grid grid-cols-3 gap-5 absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-            <div class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-          </div>
-
-          <div class="grid grid-cols-4 gap-3 my-10">
-          <?php 
+          <?php
+          
               include '../PHP/loadAllVans.php'; 
 
               foreach ($furgos as $furgo){
@@ -106,12 +48,14 @@
               }
 
             ?>
-          </div>
-          
-          <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-            <div class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-          </div>
+            
         </div>
+            
+        <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+          <div class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+        </div>
+
       </div>
+    </div>
 </body>
 </html>
